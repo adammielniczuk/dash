fde0c260883339644d4b700c179ec7663caeffb1
Adam <adammielniczuk12@gmail.com>
2023-06-03 14:07:02 +0200

    dsfjknds

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,7 +1,7 @@
 from shiny import App, render, ui
 
 app_ui = ui.page_fluid(
-    ui.h2("Hello a!"),
+    ui.h2("Hello akadsubbhjkdcvbmhdsf!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
 )
