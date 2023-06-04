601a3766b062757b86dc4f8569f7d17e0f0cc8b2
krzysztof-m-w <krzysztof.m.weber@gmail.com>
2023-06-04 10:08:00 +0200

    add map widget

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,9 +1,16 @@
 from shiny import App, render, ui
+from shinywidgets import output_widget, render_widget
+import plotly.graph_objects as go
+import plotly.io as pio
+from map import map_fig
+
+
 
 app_ui = ui.page_fluid(
     ui.h2("Hello akadsubbhjkdcvbmhdsf!"),
     ui.input_slider("n", "N", 0, 100, 20),
     ui.output_text_verbatim("txt"),
+    output_widget("plot")
 )
 
 
@@ -12,6 +19,16 @@ def server(input, output, session):
     @render.text
     def txt():
         return f"n*2 is {input.n() * 2}"
+    
+    
+    @output
+    @render_widget
+    def plot():       
+        return map_fig  
+    
 
 
 app = App(app_ui, server)
+
+if __name__ == '__main__':
+    app.run()
