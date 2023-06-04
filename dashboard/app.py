131ee31af6db87328db46fbcb3637d3328a7b0e0
Adam <adammielniczuk12@gmail.com>
2023-06-04 19:09:24 +0200

    fix?

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,9 +1,9 @@
 from shiny import App, render, ui
 import os
 import micropip
-
-micropip.install("shinywidgets")
-
+async def l():
+    await micropip.install("shinywidgets")
+l()
 from shinywidgets import output_widget, render_widget
 import plotly.graph_objects as go
 import plotly.io as pio
