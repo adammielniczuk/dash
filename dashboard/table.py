5c803c1e7af2fae68f6ddc73de5e628975c63091
Adam <adammielniczuk12@gmail.com>
2023-06-09 21:36:30 +0200

    changes

--- dashboard/table.py
+++ dashboard/table.py
@@ -1,9 +1,9 @@
 import pandas as pd
-
+import pyodide
 
 raw_path='https://raw.githubusercontent.com/adammielniczuk/dash/main/dashboard/data'
 
-df=pd.read_csv((raw_path+"/city_late.csv"), sep=';')
+df=pd.read_csv(pyodide.htpp.open_url(raw_path+"/city_late.csv"), sep=';')
 
 df['average % of trains on time'] = df.iloc[:, 1:].mean(axis=1)*100
 df = df[['station', 'average % of trains on time']]
